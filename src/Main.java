1f690b76f76d695b45f4263cc8a49fc2f7634c0b
IliaNovikov <ilja.nowikow2013@yandex.ru>
2023-06-21 20:28:09 +0300

    Interfaces into separate files

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Worker.OnTaskDoneListener listener = System.out::println;
-        Worker.OnTaskErrorListener listener1 = System.out::println;
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listener1 = System.out::println;
 
         Worker worker = new Worker(listener, listener1);
 
